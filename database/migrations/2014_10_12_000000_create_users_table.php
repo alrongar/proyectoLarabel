ad3c439f60b626ecb0b540fe4ad9338c1544fe44
alrongar <alejandroroncerogarrido@gmail.com>
2024-10-21 13:43:08 +0200

    añadir rol de organizador

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['admin', 'user']);
+            $table->enum('role', ['admin', 'user', 'organizer']);
             $table->string('profile_picture')->nullable();
             $table->tinyInteger('actived')->default(1);
             $table->tinyInteger('email_confirmed')->default(0);
