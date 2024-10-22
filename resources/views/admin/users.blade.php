3a83bba72dd2f90c08641713c947a89e85148094
crislr11 <cistianlr@gmail.com>
2024-10-22 17:11:12 +0200

    añadir los botones de añadir / eliminar a cada usuario desde el perfil de administrador

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -28,32 +28,24 @@
                                     <td>{{ $user->name }}</td>
                                     <td>{{ $user->email }}</td>
                                     <td>
-                                        <!-- Mostrar botón de "Activar" o "Desactivar" según el estado -->
-                                        @if($user->actived)
-                                            <form action="{{ route('users.deactivate', $user->id) }}" method="POST">
-                                                @csrf
-                                                @method('PUT')
-                                                <button type="submit" class="btn btn-warning">Desactivar</button>
-                                            </form>
-                                        @else
+                                        <!-- Mostrar botón de "Activar" solo para usuarios desactivados -->
+                                        @if(!$user->actived)
                                             <form action="{{ route('users.activate', $user->id) }}" method="POST">
                                                 @csrf
                                                 @method('PUT')
                                                 <button type="submit" class="btn btn-success">Activar</button>
                                             </form>
+                                        @else
+                                            <!-- Mostrar botón de "Borrar" solo para usuarios activados -->
+                                            <form action="{{ route('users.destroy', $user->id) }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger">Borrar</button>
+                                            </form>
                                         @endif
-                                    </td>
-                                    <td>
-                                        <!-- Botón de borrar siempre disponible -->
-                                        <form action="{{ route('users.destroy', $user->id) }}" method="POST">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-danger">Borrar</button>
-                                        </form>
                                     </td>
                                 </tr>
                             @endforeach
-
                         </tbody>
                     </table>
 
